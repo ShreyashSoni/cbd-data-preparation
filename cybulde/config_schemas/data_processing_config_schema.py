9ceb09c35d722d13b3cf6c0bd5e9c672d12f2c63
ShreyashSoni <yashsoni96@gmail.com>
2024-05-15 18:04:34 +0530

    set up data readers and configs

--- cybulde/config_schemas/data_processing_config_schema.py
+++ cybulde/config_schemas/data_processing_config_schema.py
@@ -1,23 +1,33 @@
 from hydra.core.config_store import ConfigStore
-from pydantic.dataclasses import dataclass
 from omegaconf import MISSING
+from pydantic.dataclasses import dataclass
 
-from cybulde.config_schemas.infrastructure.gcp_schema import GCPConfig
+from cybulde.config_schemas.data_processing import dataset_readers_schema
+from cybulde.config_schemas.infrastructure import gcp_schema
 
 
 @dataclass
 class DataProcessingConfig:
     version: str = MISSING
     data_local_save_dir: str = "./data/raw"
-    dvc_remote_repo: str = "https://github.com/ShreyashSoni/cbd_data.git"
+    dvc_remote_repo: str = "https://github.com/emkademy/cybulde-data.git"
     dvc_data_folder: str = "data/raw"
-    github_user_name: str = "ShreyashSoni"
-    github_access_token_secret_id: str = "cbd-data-github-access-token"
+    github_user_name: str = "emkademy"
+    github_access_token_secret_id: str = "cybulde-data-github-access-token"
 
-    infrastructure = GCPConfig()
+    infrastructure: gcp_schema.GCPConfig = gcp_schema.GCPConfig()
+    dataset_reader_manager: dataset_readers_schema.DatasetReaderManagerConfig = MISSING
 
+    processed_data_save_dir: str = MISSING
+
+    run_tag: str = "default_run"
+
+    min_nrof_words: int = 2
 
 
 def setup_config() -> None:
+    gcp_schema.setup_config()
+    dataset_readers_schema.setup_config()
+
     cs = ConfigStore.instance()
     cs.store(name="data_processing_config_schema", node=DataProcessingConfig)
